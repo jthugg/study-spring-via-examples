e959624e20452027270474dbd141ae283f1ab578
jthugg <jthug@naver.com>
2023-02-09 23:11:27 +0900

    Chore BoardPostRepository.java
    annotate @Repository

--- src/main/java/com/neoTheCow/Example/repository/BoardPostRepository.java
+++ src/main/java/com/neoTheCow/Example/repository/BoardPostRepository.java
@@ -4,7 +4,9 @@ import com.neoTheCow.Example.entity.BoardPost;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BoardPostRepository extends JpaRepository<BoardPost, Long> {
 
 	/**
